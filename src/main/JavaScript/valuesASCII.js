6d458a4977837cfaa3c009f8c3046586db04ab80
Durjoy <da-durjoy@outlook.com>
2022-05-03 22:52:36 +0600

    ASCII VALUES

--- src/main/JavaScript/valuesASCII.js
+++ src/main/JavaScript/valuesASCII.js
@@ -1,8 +1,8 @@
 function PrintASCIIvalues() {
     console.log("==============ASCII VALUES==============")
     Array.from(Array(127).keys()).slice(32).map(e=>{
-       // console.log(String.fromCharCode(e));
-        console.log(e);
+        console.log(String.fromCharCode(e));
+       // console.log(e);
     });
 }
 PrintASCIIvalues();
