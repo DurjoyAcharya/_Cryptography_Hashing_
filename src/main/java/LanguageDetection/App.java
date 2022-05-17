159c46e638d28d404f2f96e4ac5ef33f66504a32
Durjoy <da-durjoy@outlook.com>
2022-05-17 21:28:24 +0600

    Finally Done

--- src/main/java/LanguageDetection/App.java
+++ src/main/java/LanguageDetection/App.java
@@ -1,15 +1,11 @@
 package LanguageDetection;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class App {
     public static void main(String[] args) {
-        //var fileProcessor=new FileProcessor();
+        var fileProcessor=new FileProcessor();
         String message="I LOVE YOU";
-        //var bool= fileProcessor.isEnglish(message);
-        //System.out.println(bool);
-       // System.out.println(fileProcessor.MatchesWords(message));
-        //Arrays.stream(message.split(" ")).forEach();
+        //System.out.println(fileProcessor.MatchesWords(message));
+        var bool= fileProcessor.isEnglish(message);
+        System.out.println(bool);
     }
 }
--- src/main/java/LanguageDetection/FileProcessor.java
+++ src/main/java/LanguageDetection/FileProcessor.java
@@ -2,25 +2,32 @@ package LanguageDetection;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FileProcessor {
+
     private List<String> list;
     private FileReader fileReader;
     private BufferedReader bufferedReader;
+
     public FileProcessor() {
         this.list=new ArrayList<>();
         try {
-            getDate();
+            getData();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private void getDate() throws IOException {
+
+    private void getData() throws IOException {
         fileReader=new FileReader(new File("src/main/java/LanguageDetection/english_words.txt"));
         bufferedReader=new BufferedReader(fileReader);
+
         String line="";
+        //I start reading one by one from this file english_words
+        //and added to arrayList<>
         while ((line=bufferedReader.readLine())!=null)
         {
             list.add(line.toUpperCase());
@@ -29,24 +36,26 @@ public class FileProcessor {
         bufferedReader.close();
 
     }
+
     public List<String> getWords()
     {
         return this.list;
     }
-    public int MatchesWords(String message)
-    {
-        int count=0;
-        message=message.toUpperCase();
-        String[] array=new String[message.split(" ").length];
-        for (int i = 0; i < array.length; i++) {
-            //array[i]=message.split(" ");
+
+    //this function is going to be maches words along with the file word and given parms
+    public int MatchesWords(String message) {
+        int count = 0;
+        message = message.toUpperCase();
+        String[] array = message.split(" ");
+           for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < getWords().size(); j++) {
+                if (array[i].equals(getWords().get(j)))
+                    count++;
+            }
         }
-        getWords().forEach(e->{
-
-        });
-
         return count;
     }
+
     public boolean isEnglish(String message)
     {
         boolean flag=false;
